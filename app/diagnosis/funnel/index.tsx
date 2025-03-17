2940cc73a7a27d831af4ced696624ceb9672189c
JJJ-un <jhjk7272@naver.com>
2025-03-17 15:35:44 +0900

    docs: 비즈니스 로직 주석 추가[FE-18]

--- app/diagnosis/funnel/index.tsx
+++ app/diagnosis/funnel/index.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ProgressBar from '@/features/diagnosis/ui/ProgressBar';
 import useProgress from '@/features/diagnosis/lib/useProgress';
-import { handleNextClick } from '@/features/diagnosis/services/progressStep';
+import { handleNextClick } from '@/features/diagnosis/services/updateProgress';
 import theme from '@/shared/styles/theme';
 
 const steps = ['1', '2', '3', '4', '5'];
--- features/diagnosis/services/updateProgress.tsx
+++ features/diagnosis/services/updateProgress.tsx
@@ -1,3 +1,4 @@
+// 다음 스텝의 이동, 진행률 업데이트 함수
 const navigateOrSetStep = (
     index: number,
     steps: string[],
@@ -5,6 +6,7 @@ const navigateOrSetStep = (
     setCurrentStep: (step: string) => void,
     setProgress: (progress: number) => void,
   ) => {
+    // 유효한 인덱스인지 검사 (0 이상, 전체 스텝 개수 미만)
     if (index >= 0 && index < steps.length) {
       const nextStep = steps[index];
       setStep(nextStep);
@@ -13,6 +15,8 @@ const navigateOrSetStep = (
     }
   };
   
+  // 전체 스텝 이동, 진행률 관리 함수 
+  // 헤더 추가 시 뒤로 이동하는 로직 추가 예정
   export const handleNextClick =
     (
       getCurrentStepIndex: () => number,
