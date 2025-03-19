d193f517fa872eb99dac4811b96ef19143a4e858
Hwangchanjin <slo0521@naver.com>
2025-03-19 20:35:42 +0900

    컴포넌트로 분리

--- app/(tabs)/records/index.tsx
+++ app/(tabs)/records/index.tsx
@@ -1,9 +1,11 @@
+// 들어가자 마자 보이는 화면
 import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from "react-native";
 import { useRouter } from 'expo-router';
+import MedicalRecordDetails from "@/features/auth/services/records/medicalRecordDetails";
+import HealthStatusDetails from "@/features/auth/services/records/healthStatusDetails";
 
 export default function RecordsIndex() {
   const router = useRouter(); 
-
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
@@ -13,26 +15,7 @@ export default function RecordsIndex() {
         }}
         >
         <Text style={styles.sectionTitle}>Medical Record</Text>
-        <View style={styles.recordItemTop}>
-          <Text style={styles.labelTop}>Date of visit</Text>
-          <Text style={styles.valueTop}>2024.11.24</Text>
-        </View>
-        <View style={styles.recordItemTop}>
-          <Text style={styles.labelTop}>Visiting Hospital</Text>
-          <Text style={styles.valueTop}>Hanyang Hospital</Text>
-        </View>
-        <View style={styles.recordItemTop}>
-          <Text style={styles.labelTop}>Medical Subjects</Text>
-          <Text style={styles.valueTop}>sgdfgsdgfg</Text>
-        </View>
-        <View style={styles.recordItemTop}>
-          <Text style={styles.labelTop}>Symptoms</Text>
-          <Text style={styles.valueTop}>sgdfgsdgfg</Text>
-        </View>
-        <View style={styles.cornerRadiusTop}>
-          <Text style={styles.labelTop}>Treatment Summary</Text>
-          <Text style={styles.valueTop}>asdfasdfasasdfasdfasdfasf</Text>
-        </View>
+        <MedicalRecordDetails />
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -42,23 +25,12 @@ export default function RecordsIndex() {
       }}
       >
         <Text style={styles.sectionTitle}>Health Status</Text>
-        <View style={styles.recordItemBottom}>
-          <Text style={styles.labelBottom}>Disease</Text>
-          <Text style={styles.valueBottom}>asdfasdfasd</Text>
-        </View>
-        <View style={styles.recordItemBottom}>
-          <Text style={styles.labelBottom}>Allergy</Text>
-          <Text style={styles.valueBottom}>asdfasdfasd</Text>
-        </View>
-        <View style={styles.cornerRadiusBotttom}>
-          <Text style={styles.labelBottom}>Blood pressure</Text>
-          <Text style={styles.valueBottom}>asdfasdfasd</Text>
-        </View>
+        <HealthStatusDetails />
       </TouchableOpacity>
     </SafeAreaView>
   );
 }
-
+// 요까지
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#F5F9FC", 
@@ -91,66 +63,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     marginBottom: 12, 
-  },
-  recordItemTop: {
-    backgroundColor:"#FFFFFF",
-    paddingLeft:16,
-    justifyContent: "space-between",
-    paddingVertical: 6,
-    borderBottomWidth: 1,
-    borderBottomColor: "#E5E7EB",
-  },
-  recordItemBottom: {
-    backgroundColor:"#FFFFFF",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 6,
-    borderBottomColor: "#E5E7EB",
-  },
-  cornerRadiusTop: {
-    backgroundColor:"#FFFFFF",
-    paddingLeft:16,
-    justifyContent: "space-between",
-    paddingVertical: 6,
-    borderBottomWidth: 1,
-    borderBottomColor: "#E5E7EB",
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  cornerRadiusBotttom: {
-    backgroundColor:"#FFFFFF",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 6,
-    borderBottomColor: "#E5E7EB",
-    borderBottomLeftRadius: 10,
-    borderBottomRightRadius: 10,
-  },
-  labelTop: {
-    fontSize: 16,
-    fontWeight: "400",
-    color: "#000",
-    marginBottom:4,
-    marginTop:9,
-  },
-  valueTop: {
-    fontSize: 14,
-    color: "#656565",
-    fontWeight: "400",
-    marginBottom: 9,
-  },
-  labelBottom: {
-    fontSize: 16,
-    fontWeight: "400",
-    color: "#000",
-    marginVertical: 9,
-    marginLeft:16,
-  },
-  valueBottom: {
-    fontSize: 14,
-    color: "#656565",
-    fontWeight: "400",
-    marginVertical: 9,
-    marginRight:16,
   },
 });
